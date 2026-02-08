077d15ce841929132c01481922298ab26d71243d
A-4-Atom <vcikas2626@gmail.com>
2026-02-08 22:28:48 +0530

    build logic to fetch tickets

--- TicketPlatform/Controllers/HomeController.cs
+++ TicketPlatform/Controllers/HomeController.cs
@@ -1,18 +1,41 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using TicketPlatform.Models;
 
 namespace TicketPlatform.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            // Protect this page
             if (Session["UserId"] == null)
-            {
                 return RedirectToAction("Login", "Auth");
+
+            var userId = Session["UserId"].ToString();
+
+            var url = $"http://localhost:7255/api/tickets?userId={userId}";
+
+            List<Ticket> tickets = new List<Ticket>();
+            System.Diagnostics.Debug.WriteLine("SESSION USERID = " + userId);
+            System.Diagnostics.Debug.WriteLine("API URL = " + url);
+
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    tickets = JsonConvert.DeserializeObject<List<Ticket>>(json);
+                }
             }
 
-            return View();
+            return View(tickets);
         }
     }
 }
---
+++ TicketPlatform/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketPlatform.Models
+{
+    public class Ticket
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string category { get; set; }
+        public string status { get; set; }
+        public string createdAt { get; set; }
+    }
+}
